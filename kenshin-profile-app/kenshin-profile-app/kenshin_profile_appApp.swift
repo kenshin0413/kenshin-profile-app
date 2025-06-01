0c0b65d34f661bf9121977d99e4ae8f45f9413f7
miyamotokenshin <kenshin@miyamotokenshinnoMacBook-Pro.local>
2025-06-01 12:38:04 +0900

    プロジェクトの追加

---
+++ kenshin-profile-app/kenshin-profile-app/kenshin_profile_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  kenshin_profile_appApp.swift
+//  kenshin-profile-app
+//
+//  Created by miyamotokenshin on R 7/06/01.
+//
+
+import SwiftUI
+
+@main
+struct kenshin_profile_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
