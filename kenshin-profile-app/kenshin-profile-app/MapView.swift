35f76e6dfb84446d90765f7d2d2960efe678f3e5
kenshin0413 <kenshin1234569@gmail.com>
2025-06-12 19:50:30 +0900

    Mapのスクロール無効化

--- kenshin-profile-app/kenshin-profile-app/MapView.swift
+++ kenshin-profile-app/kenshin-profile-app/MapView.swift
@@ -8,27 +8,16 @@
 import SwiftUI
 import MapKit
 
-struct Location: Identifiable {
-    let id = Double()
-    let coordinate: CLLocationCoordinate2D
-}
 
 struct MapView: View {
-    private let region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 35.5402650, longitude: 140.2634765),
-        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-    )
-    
-    private let pinLocation = Location(coordinate: CLLocationCoordinate2D(latitude: 35.5402650, longitude: 140.2634765))
-    
     var body: some View {
-        Map(
-            coordinateRegion: .constant(region),
-            interactionModes: [],
-            annotationItems: [pinLocation]
-        ) { location in
-            MapPin(coordinate: location.coordinate, tint: .red)
-        }
+        Map(initialPosition: .region(region), interactionModes: [])
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 35.5402650, longitude: 140.2634765),
+            span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
     }
 }
 #Preview {
